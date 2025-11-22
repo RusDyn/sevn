e2e31382c7b68dce3c3a4af4035268df3b2fc6dc
Alex <dyn.see@gmail.com>
2025-11-22 16:37:56 +0000

    Fix example asset paths in Sevn tabs

--- apps/sevn/app/(tabs)/explore.tsx
+++ apps/sevn/app/(tabs)/explore.tsx
@@ -60,7 +60,7 @@ export default function TabTwoScreen() {
           different screen densities
         </ThemedText>
         <Image
-          source={require('../../../../example/assets/images/react-logo.png')}
+          source={require('../../../example/assets/images/react-logo.png')}
           style={{ width: 100, height: 100, alignSelf: 'center' }}
         />
         <ExternalLink href="https://reactnative.dev/docs/images">
